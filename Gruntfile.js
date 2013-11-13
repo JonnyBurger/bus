02de4a9af15528a7637b42f839c81edb06ff8d3b
Jonny Burger <jonathanburger11@gmail.com>
2013-11-13 19:25:58 +0100

    Moving files

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,9 +30,23 @@ module.exports = function(grunt) {
 					"style.css": "style.less"
 				}
 			}
+		},
+		'ftp-deploy': {
+			build: {
+				auth: {
+					host: 'bus.herobo.com',
+					port: 21,
+					authKey: 'default'
+				},
+				src: '/Users/jonnyburger/Desktop/bus',
+				dest: '/public_html',
+				exclusions: ['node_modules', '.git', '.ftppass', 'Gruntfile.js', 'index.js', 'package.json', 'style.less', '.DS_Store']
+			}
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-ftp-deploy');
 	grunt.registerTask('default', ['watch']);
+	grunt.registerTask('deploy', ['ftp-deploy']);
 }
--- app.js
+++ app.js
@@ -143,22 +143,22 @@ bus.helpers = {
 			return {unit: '', 			number: 'jetzt', available: true}
 		}
 		if (seconds < 1) {
-			return {unit: 'Sekunde', 	number: 1}
+			return {unit: 's', 	number: 1}
 		}
 		if (seconds < 60) {
-			return {unit: 'Sekunden', 	number: Math.floor(seconds)}
+			return {unit: 's', 	number: Math.floor(seconds)}
 		}
 		if (seconds < 120) {
-			return {unit: 'Minute', 	number: 1}
+			return {unit: 'min', 	number: 1}
 		}
 		if (minutes < 60) {
-			return {unit: 'Minuten', 	number: Math.floor(seconds/60)}
+			return {unit: 'min', 	number: Math.floor(seconds/60)}
 		}
 		if (minutes < 120) {
-			return {unit: 'Stunde', 	number: 1}
+			return {unit: 'h', 	number: 1}
 		}
 		else {
-			return {unit: 'Stunden', 	number: Math.floor(minutes/60)}
+			return {unit: 'h', 	number: Math.floor(minutes/60)}
 		}
 	},
 	parseStation: function(station) {
@@ -174,70 +174,73 @@ bus.helpers = {
 	},
 	parseType: function(type) {
 		if (type == 'NFO') {
-			return 'Trolleybus'
+			return 'Du fährst mit einem Trolleybus.'
 		}
 		if (type == 'NFB') {
-			return 'Niederflurbus'
+			return 'Du fährst mit einem Niederflurbus.'
 		}
 		if (type == 'TRO') {
-			return 'Trolleybus'
+			return 'Du fährst mit einem Trolleybus.'
 		}
 		if (type.substr(0,1) == 'S') {
-			return 'S-Bahn'
+			return 'Dies ist eine S-Bahn-Linie.'
 		}
 		if (type == 'RE') {
-			return 'RegionalExpress'
+			return 'Dieser Zug ist ein RegioExpress.'
 		}
 		if (type == 'R') {
-			return 'Regionalzug'
+			return 'Dieser Zug ist ein Regionalzug.'
 		}
 		if (type == 'BUS') {
-			return 'Bus';
+			return '';
 		}
 		if (type == 'T') {
-			return 'Tram';
+			return '';
 		}
 		if (type == 'NFT') {
-			return 'Niederflurtram';
+			return 'Es handelt sich um ein rollstuhlfreundliches Niederflurtram.';
 		}
 		if (type == 'IR') {
-			return 'Interregio'
+			return 'Der Zug ist ein Interregio.'
 		}
 		if (type == 'EC') {
-			return 'EuroCity'
+			return 'Dies ist eine EuroCity-Linie.'
 		}
 		if (type == 'ICN') {
-			return 'InterCity Neigezug'
+			return 'Zuggattung: InterCity Neigezug.'
 		}
 		if (type == 'BAT') {
-			return 'Schiff'
+			return ''
 		}
 		if (type == 'TGV') {
-			return 'Train à Grande Vitèsse (TGV)'
+			return 'Dies ist ein TGV-Zug.'
 		}
 		if (type == 'ICE') {
-			return 'InterCity Express'
+			return 'Dieser Zug ist ein InterCity Express.'
 		}
 		if (type == 'RJ') {
-			return 'RailJet'
+			return 'Dieser Zug ist ein RailJet.'
 		}
 		if (type == 'EN') {
-			return 'EuroNight'
+			return 'Dies ist ein Nachtreisezug.'
 		}
 		if (type == 'CNL') {
-			return 'City Night Line'
+			return 'Zuggattung: City Night Line.'
 		}
 		if (type == 'SN') {
-			return 'S-Bahn Nachtlinie'
+			return 'Dies ist eine S-Bahn Nachtlinie.'
 		}
 		if (type == 'D') {
-			return 'Schnellzug'
+			return 'Dies ist ein Schnellzug.'
 		}
 		if (type == 'EXT') {
-			return 'Extrazug'
+			return 'Achtung: Dies ist Extrazug.'
 		}
 		if (type == 'LB') {
-			return 'Luftseilbahn'
+			return 'Es handelt sich um eine Luftseilbahn.'
+		}
+		if (type == 'N' || type == 'P') {
+			return 'Dies ist ein Nahverkehrzug.'
 		}
 		return type;
 	},
@@ -246,19 +249,28 @@ bus.helpers = {
 		return categories[bus.categoryCode];
 	},
 	getTypeString: function(bus) {
-		var categories = [null, 'Dein Zug', 'Dein Zug', 'Dein Zug', 'Dein Schiff', 'Dein Zug', 'Dein Bus', 'Deine Bahn', 'Train', 'Dein Tram'];
-		return categories[bus.categoryCode]
+		var categories = [null, 'Dein Zug', 'Dein Zug', 'Dein Zug', 'Dein Schiff', 'Dein Zug', 'Dein Bus', 'Deine Bahn', 'Dein Zug', 'Dein Tram'];
+		return categories[bus.categoryCode];
 	},
-	updateTimeLeft: function(id, until, categoryCode) {
+	getTypeStringPlural: function(bus) {
+		var categories = [null, 'Züge', 'Züge', 'Züge', 'Schiffe', 'Züge', 'Busse', 'Bahnen', 'Züge', 'Tramme'];
+		return categories[bus.categoryCode];
+	},
+	getTypeStringOneMore: function(bus) {
+		var categories = [null, 'Ein weiterer Zug', 'Ein weiterer Zug', 'Der nächste Zug', 'Das nächste Schiff', 'Ein weiterer Zug', 'Ein weiterer Bus', 'Die nächste Bahn', 'Der nächste Zug', 'Ein weiteres Tram'];
+		return categories[bus.categoryCode];
+	},
+	updateTimeLeft: function(id, until, categoryCode, date) {
 		if (until > 0) {
 			setTimeout(function() {
-				bus.helpers.updateLabel(id, until, 1000, categoryCode)
+				bus.helpers.updateLabel(id, until, 1000, categoryCode, date)
 			}, 1000)
 		}
 	},
-	updateLabel: function(id, until, subtract, categoryCode) {
-		until -= subtract;
-		var time = bus.helpers.timeLeftToNumber(until)
+	updateLabel: function(id, until, subtract, categoryCode, date) {
+		console.log(date);
+		var until = bus.ui.fixTimeZone(date);
+		var time = bus.helpers.timeLeftToNumber(until);
 		var html = _.template(bus.templates.time, {time: time, id: id, category: categoryCode});
 		var query = document.querySelector('[data-id="' + id + '"]');
 		$(query).replaceWith(html);
@@ -267,25 +279,25 @@ bus.helpers = {
 		}
 		else if (until < 60001) {
 			setTimeout(function() {
-				bus.helpers.updateLabel(id, until, 1000, categoryCode)
+				bus.helpers.updateLabel(id, until, 1000, categoryCode, date)
 			}, 1000);
 			return;
 		}
 		else if (60000 < until < 120000) {
 			setTimeout(function() {
-				bus.helpers.updateLabel(id, until, until - 60000, categoryCode)
+				bus.helpers.updateLabel(id, until, until - 60000, categoryCode, date)
 			}, until - 60000);
 			return;
 		}
 		else if (119999 < until < 3600000) {
 			setTimeout(function() {
-				bus.helpers.updateLabel(id, until, 60000, categoryCode)
+				bus.helpers.updateLabel(id, until, 60000, categoryCode, date)
 			}, 60000);
 			return;
 		}
 		else {
 			setTimeout(function() {
-				bus.helpers.updateLabel(id, until, 3600000, categoryCode)
+				bus.helpers.updateLabel(id, until, 3600000, categoryCode, date)
 			}, 3600000)
 			return;
 		}
@@ -325,7 +337,8 @@ bus.ui = {
 	createRide: function (data) {
 		var random = Math.floor(Math.random()*10000000),
 			html = _.template(bus.templates.ride, {data: data, random: random});
-		$('.tile').remove()
+		$('.tile').addClass('invisible');
+		$('header').addClass('enable-back');
 		$('#tiles').append(html);
 		bus.ui.updateRide({id: random, data: data});
 	},
@@ -402,6 +415,12 @@ bus.ui = {
 		.on('click', '.busreveal',		bus.ui.revealBoard 	)
 		.on('click', '.search-stop', 	bus.ui.searchStop 	)
 		.on('click', '.boarding', 		bus.ui.boarding		)
+		.on('click', '#back',			bus.ui.back 		)
+	},
+	back: function() {
+		$('header').removeClass('enable-back');
+		$('.tile:visible').remove();
+		$('.tile').removeClass('invisible');
 	},
 	searchStop: function() {
 		var input = prompt('Station eingeben:');
@@ -445,8 +464,11 @@ bus.ui = {
 		}
 		else {
 			var a = (new Date(date.substr(0, 19)) - new Date());
-			if (/WebKit/.test(navigator.userAgent)) {
+			if (/WebKit/.test(navigator.userAgent) && date.substr(20) == '0200') {
 				a -= 120*60*1000;
+			}
+			else if (/WebKit/.test(navigator.userAgent) && date.substr(20) == '0100') {
+				a -= 60*60*1000;
 			}
 		}
 		return a;
@@ -455,27 +477,25 @@ bus.ui = {
 bus.templates = {
 	busstop: [
 		"<div class='tile busstop-tile'>",
-			"<h2>Du bist bei</h2>",
+			"<h2>Dein Standort</h2>",
 			"<h1><%= helpers.parseStation(stop.name) %></h1>",
 			"<div id='map'>",
 				"<img onload=$(\'.busstop-tile\').addClass(\'ready\') class='map' src='//maps.googleapis.com/maps/api/staticmap?center=<%= stop.x || stop.coordinate.x %>,<%= stop.y || stop.coordinate.y %>&zoom=15&size=<%= document.body.clientWidth - 10 %>x180&sensor=true&zoom=14&scale=2'>",
 				"<div class='blue-circle'></div>",
 				"<div class='bus-symbol'><div class='bus-icon'></div></div>",
 			"</div>",
-			"<div class='list'>",
-				"<div class='reveal reveal-map'>Karte anzeigen</div>",
-			"</div>",
+			"<table class='buttons'>",
+				"<tr>",
+					"<td><button class='reveal-map'>Karte anzeigen</button></td>",
+					"<td><button class='reveal-stops'>Andere Haltestellen</button></td>",
+				"</tr>",
+			"</table>",
 			"<% if (nearby.length != 0) { %>",
-				"<div class='list'>",
-					"<div class='reveal reveal-stops'>Andere Haltestellen</div>",
-				"</div>",
 				"<div class='list nearby-busstops'>",
 					"<% _.each(nearby, function (stop) { %>",
 						"<div class='option busstop' data-id='<%= stop.id %>' data-name='<%= stop.name %>' data-x='<%= stop.coordinate.x %>' data-y='<%= stop.coordinate.y %>'><%= helpers.parseStation(stop.name) %></div>",
 					"<% }); %>",
-					"<div class='list'>",
-						"<div class='reveal search-stop'>Haltestelle suchen</div>",
-					"</div>",
+					"<button class='search-stop'>Haltestelle suchen</button>",
 				"</div>",
 			"<% } %>",
 		"</div>"
@@ -504,34 +524,28 @@ bus.templates = {
 						"<div class='destination'>",
 							"<% var first = line.splice(0,1)[0]; %>",
 							"<% var time = helpers.timeLeftToNumber(first.stop.until), id = Math.floor(Math.random()*1000000) %>",
-							"<% helpers.updateTimeLeft(id, first.stop.until, first.categoryCode) %>",
-							"<table>",
-								"<td>",
-									"<%= _.template(bus.templates.time, {time: time, id: id, category: first.categoryCode}) %>",
-								"</td>",
-								"<td>",
-									"<% if (line.length != 0) { %>",
-										"<div id='moreconnections'>",
-											"<h2>Weitere Verbindungen</h2>",
-											"<ul>",
-												"<% _.each(line, function(bus) { %>",
-													"<li>",
-														"<span><%= helpers.getTimeUntilFromDate(bus.stop.until) %> <span>(<%= helpers.timeLeftToString(bus.stop.until) %>)</span></span>",
-													"</li>",
-												"<% }); %>",
-											"</ul>",
-										"</div>",
+							"<% helpers.updateTimeLeft(id, first.stop.until, first.categoryCode, first.stop.departure) %>",
+							"<%= _.template(bus.templates.time, {time: time, id: id, category: first.categoryCode}) %>",
+							"<p><%= bus.helpers.parseType(first.category) %>",
+							"<% if (line.length != 0) { %>",
+									"<% if (line.length == 1) { %>",
+										"<%= helpers.getTypeStringOneMore({categoryCode: first.categoryCode}) %> fährt um <%= helpers.getTimeUntilFromDate(line[0].stop.until) %>.",
+									"<% } else { %>",
+										"Weitere <%= helpers.getTypeStringPlural({categoryCode: first.categoryCode}) %> fahren um ",
+										"<% _.each(line, function(bus, key) { %>",
+											"<%= helpers.getTimeUntilFromDate(bus.stop.until) %><% if (key == line.length -1 ) { %>.<% }%><% if (key+2 < line.length ) { %>,<% }%>",
+											"<% if (key == line.length -2 ) { %>und<% }%>",
+										"<% }); %>",
 									"<% } %>",
-									"<div class='list'>",
-										"<div class='reveal boarding' data-start='<%= first.stop.location.id %>' data-end='<%= first.to %>' data-date='<%= first.stop.departure %>'>Einsteigen</div>",
-									"</div>",
-								"</td>",
-							"</table>",
+							"<% } %>",
+							"</p>",
+							"<div class='list'>",
+								"<button class='reveal boarding' data-start='<%= first.stop.location.id %>' data-end='<%= first.to %>' data-date='<%= first.stop.departure %>'>Einsteigen</button>",
+							"</div>",
 						"</div>",
 					"<% }); %>",
 				"<% }); %>",
 			"</div>",
-			"<div class='dot'></div>",
 		"</div>"
 	].join('\n'),
 	time: [
@@ -540,25 +554,27 @@ bus.templates = {
 				"<div class='time-wrapper'>",
 					"<h2>Abfahrt in</h2>",
 					"<h1>",
-						'<%= time.number %> <p><%= time.unit %></p>',
+						'<%= time.number %><span><%= time.unit %></span>',
 					"</h1>",
 				"</div>",
 			"<% } else { %>",
 				"<% var cat = bus.helpers.getTypeIdentifier({categoryCode: category});", 
 				"var categoryString = bus.helpers.getTypeString({categoryCode: category}); %>",
-				"<h2><%= categoryString %> ist da!</h2><br>",
-				"<% if (cat == 'bus') { %>",
-					"<img src='img/busblack.svg' class='slidein'>",
-				"<% } %>",
-				"<% if (cat == 'tram') { %>",
-					"<img src='img/tram.svg' class='scalein'>",
-				"<% } %>",
-				"<% if (cat == 'ship') { %>",
-					"<img src='img/ship.svg' class='slidein'>",
-				"<% } %>",
-				"<% if (cat == 'train') { %>",
-					"<img src='img/trainblack.svg' class='slidein'></img>",
-				"<% } %>",
+				"<h2 class='center'><%= categoryString %> ist da!</h2><br>",
+				"<div style='text-align: center'>",
+					"<% if (cat == 'bus') { %>",
+						"<img src='img/busblack.svg' class='slidein'>",
+					"<% } %>",
+					"<% if (cat == 'tram') { %>",
+						"<img src='img/tram.svg' class='scalein'>",
+					"<% } %>",
+					"<% if (cat == 'ship') { %>",
+						"<img src='img/ship.svg' class='slidein'>",
+					"<% } %>",
+					"<% if (cat == 'train') { %>",
+						"<img src='img/trainblack.svg' class='slidein'></img>",
+					"<% } %>",
+				"</div>",
 			"<% } %>",
 		"</div>"
 
@@ -590,8 +606,9 @@ bus.templates = {
 		"</div>"
 	].join('\n'),
 	rideinfos: [
+		"<h2>Zeit verbleibend bis Endstation</h2>",
 		"<h1><%= time.number %> <small><%= time.unit %></small></h1>",
-		"<p><span><%= Math.round(distanceridden*100)/100 %>/<%= Math.round(totaldistance*100)/100 %>km</span> - <span><%= Math.round(distanceridden/totaldistance*10000)/100 %>%</span></p>"
+		"<h2><span><%= Math.round(distanceridden*100)/100 %>/<%= Math.round(totaldistance*100)/100 %>km</span> - <span><%= Math.round(distanceridden/totaldistance*10000)/100 %>%</span></h2>"
 	].join('\n')
 }
 Number.prototype.toRad = function() { return this * (Math.PI / 180); };
